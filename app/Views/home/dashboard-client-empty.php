9b22c4034f8fd0989b126b1cddc91e291cab0a3b
muddathiroshey <muddathirmohali@gmail.com>
2026-04-25 20:21:34 +0300

    added external css

--- app/Views/home/dashboard-client-empty.php
+++ app/Views/home/dashboard-client-empty.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Client Dashboard — Nexus</title>
-<link rel="stylesheet" href="../../public/assets/css/style.css">
-<link rel="stylesheet" href="../../public/assets/css/dashboard-client-empty.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/dashboard-client-empty.css">
 </head>
 <body>
 
--- app/Views/home/profile-setup-client.php
+++ app/Views/home/profile-setup-client.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Complete Your Profile — Nexus</title>
-<link rel="stylesheet" href="../../../public/assets/css/style.css">
-<link rel="stylesheet" href="../../../public/assets/css/profile-setup-client.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/profile-setup-client.css">
 </head>
 <body>
 
