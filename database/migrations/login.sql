c9f88f9cd8b3dce56504ac511dc38c0ef4905e1d
MZy-77 <mohamedezz22999@gmail.com>
2026-05-11 15:48:23 +0300

    more

--- database/migrations/login.sql
+++
@@ -1,8 +0,0 @@
-use freelance_marketplace;
-create table userData(
-    id int primary key auto_increment,
-    user_email varchar(255) unique not null,
-    user_password varchar(255) not null,
-    user_name varchar(255) not null,
-    user_role enum('Freelancer', 'Client', 'Admin','Arbitrator') default 'Client' 
-);
